cfa2d5cb1db4e35ce55519816f088cc2526cc8be
Huynh Hai <haihcce160053@fpt.edu.vn>
2023-03-08 20:06:41 +0700

    first commit

---
+++ DietarySupplementalShopApp/frmAccountManagement.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace DietarySupplementalShopApp
+{
+    partial class frmAccountManagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmAccountManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "frmAccountManagement";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmAccountManagement";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
